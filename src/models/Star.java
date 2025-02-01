87aa575e66aac960b0232ccf254e4346f3b52e72
Yola <jolamajcher12@gmail.com>
2025-02-01 22:10:00 +0100

    Basic variables + new classes

--- src/models/Star.java
+++ src/models/Star.java
@@ -1,4 +1,18 @@
 package src.models;
 
 public class Star {
+    private String name;
+    private String catalog_name;
+    private Declination declination;
+    private RightAscension right_ascension;
+    private double apparent_magnitude;
+    private double absolute_magnitude;
+    private double distance; // in light years
+    private Constellation constellation;
+    private String hemisphere;
+    private double temperature;
+    private double mass;
+
+
+
 }
